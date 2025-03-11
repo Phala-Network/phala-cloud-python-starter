7664c0c7d11ea92b6ef843c956fdb6cff7dc2492
Leechael Yim <yanleech@gmail.com>
2025-03-12 06:18:49 +0800

    update sdk

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from fastapi import FastAPI
+from fastapi import FastAPI, JSONResponse
 from dstack_sdk import AsyncTappdClient
+from dstack_sdk.ethereum import to_account
+from dstack_sdk.solana import to_keypair
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def get_info():
+    client = AsyncTappdClient()
+    info = await client.info()
+    return JSONResponse(content=info)
 
 @app.get("/tdx_quote")
 async def tdx_quote():
@@ -22,3 +26,17 @@ async def derive_key():
     client = AsyncTappdClient()
     result = await client.derive_key('test')
     return result
+
+@app.get('/eth_account')
+async def eth_account():
+    client = AsyncTappdClient()
+    result = await client.derive_key('test')
+    account = to_account(result)
+    return { 'address': account.address }
+
+@app.get('/sol_account')
+async def sol_account():
+    client = AsyncTappdClient()
+    result = await client.derive_key('test')
+    keypair = to_keypair(result)
+    return { 'address': str(keypair.pubkey()) }
